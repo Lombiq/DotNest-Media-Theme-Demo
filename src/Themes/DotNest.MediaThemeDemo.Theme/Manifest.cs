f9e12155e88d528d5c928e8d2d7053bc3f6504a1
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2023-01-24 15:11:37 +0100

    Media theme done

---
+++ src/Themes/DotNest.MediaThemeDemo.Theme/Manifest.cs
@@ -0,0 +1,15 @@
+using Lombiq.Hosting.MediaTheme.Bridge.Constants;
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "DotNest Media Theme Demo Theme",
+    Author = "Lombiq Technologies",
+    Version = "0.0.1",
+    Website = "https://github.com/Lombiq/DotNest-Core-SDK",
+    Description = "A sample DotNest theme for local development. It must be packaged and used as a Media Theme on DotNest.",
+    BaseTheme = "TheBlogTheme",
+    Dependencies = new[]
+    {
+        FeatureNames.MediaThemeBridge
+    }
+)]
